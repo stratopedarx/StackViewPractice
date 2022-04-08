3c18abb298ea7a81ba0635aa8308bc20292b18a2
Sergey Lobanov <Sergey.Lobanov@stacksports.com>
2022-04-08 11:00:15 +0300

    Initial Commit

---
+++ StackViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewPractice
+//
+//  Created by Sergey Lobanov on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
